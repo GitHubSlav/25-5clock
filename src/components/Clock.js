403b727dd15fcfed5c0fb21639701ef0618172b1
Arthur McNuke <borodoslav@gmail.com>
2022-06-21 13:24:18 +0700

    Added 'Start' and 'Reset' buttons to Clock

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import Button from './Button.js'
+
 import '../styling/Clock.css';
+import '../styling/Button.css';
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sessionMin: 25,
+      breakMin: 5,
+      timerSec: 1500,
+      isOnSession: true,
+      isTicking: false
+    };
+  }
   render() {
     return (
       <div id="clock">
         <div id="upper-panel">
+          <div id="play-pause-restart">
+            <Button
+              iconClass={this.state.isTicking ? "fa fa-pause": "fa fa-play"}
+              text={this.state.isTicking ? "Pause" : "Start"}
+              onClick={() => console.log("click")}
+            />
+            <Button
+              iconClass="fa fa-refresh"
+              text=" Reset"
+              onClick={() => console.log("click")}
+            />
+          </div>
         </div>
-
         <div id="lower-panel">
+          
         </div>
       </div>
     );
