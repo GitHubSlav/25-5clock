6a511839227d21dcc60fb3cd563907a5dd639c96
Arthur McNuke <borodoslav@gmail.com>
2022-06-21 13:45:30 +0700

    Added 'session/break' label and remaining time to 'upper-panel'

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -16,10 +16,27 @@ class Clock extends React.Component {
       isTicking: false
     };
   }
+
+  toMMSS(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  }
+
   render() {
     return (
       <div id="clock">
         <div id="upper-panel">
+          <div id="timer-label" className="shadowed-text">
+            {this.state.isOnSession ? "Session" : "Break"}
+          </div>
+          <div id="time-left" className="shadowed-text">
+            {this.toMMSS(this.state.timerSec)}
+          </div>
           <div id="play-pause-restart">
             <Button
               iconClass={this.state.isTicking ? "fa fa-pause": "fa fa-play"}
@@ -34,7 +51,7 @@ class Clock extends React.Component {
           </div>
         </div>
         <div id="lower-panel">
-          
+
         </div>
       </div>
     );
