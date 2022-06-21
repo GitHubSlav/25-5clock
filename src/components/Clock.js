de2123fdc8d1a7d9663b4eeac5f443ad66e9f00a
Arthur McNuke <borodoslav@gmail.com>
2022-06-21 12:36:36 +0700

    Added Clock component and stylesheet for it

---
+++ src/components/Clock.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styling/Clock.css';
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <div id="clock">
+      </div>
+    );
+  }
+}
+
+export default Clock;
