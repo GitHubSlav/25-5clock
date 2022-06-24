dfa564ee51db168ebc801a04d64b3382b7f2bb2e
Arthur McNuke <borodoslav@gmail.com>
2022-06-24 13:42:03 +0700

    Fixed hardcoded increment values fro ArrowButton in LengthHandler

--- src/components/LengthHandler.js
+++ src/components/LengthHandler.js
@@ -10,14 +10,14 @@ function LengthHandler(props){
                     arrowIcon="fa fa-arrow-up" 
                     onClick={props.arrowClick}
                     className="arrow-up"
-                    increment={1}
+                    increment={props.valueUp}
                 />
                 <div>{props.time + " min"}</div>
                 <ArrowButton 
                     arrowIcon="fa fa-arrow-down" 
                     onClick={props.arrowClick}
                     className="arrow-down"
-                    increment={-1}
+                    increment={props.valueDown}
                 />
           </div>
         </div>
